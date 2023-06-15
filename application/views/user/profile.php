8dda38e60b02f21a342e20a8273e2d1387ba7206
Ankangh001 <Ankanghosh010@gmail.com>
2023-06-16 03:42:32 +0530

    update by ankan

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -1,4 +1,8 @@
 <?php
+// echo "<pre>";
+// print_r($res);
+// die;
+
 $this->load->view('user/includes/header');
 ?>
 
@@ -14,12 +18,12 @@ $this->load->view('user/includes/header');
           <li class="nav-item">
             <a class="nav-link active" href="<?=base_url('user/')?>profile"><i class="bx bx-user me-1"></i> Personal Information</a>
           </li>
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <a class="nav-link" href="<?=base_url('user/')?>account-info"><i class="bx bx-info-circle me-1"></i> Account Information</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="<?=base_url('user/')?>account-security"><i class="bx bx-cog me-1"></i> Security</a>
-          </li>
+          </li> -->
           <li class="nav-item">
             <a class="nav-link" href="<?=base_url('user/')?>account-kyc"><i class="bx bx-link-alt me-1"></i> KYC</a>
           </li>
@@ -27,7 +31,7 @@ $this->load->view('user/includes/header');
         <div class="card mb-4">
           <h5 class="card-header">Profile Details</h5>
           <!-- Account -->
-          <div class="card-body">
+          <!-- <div class="card-body">
             <div class="d-flex align-items-start align-items-sm-center gap-4">
               <img src="<?=base_url('assets/user/assets/img/')?>avatars/1.png" alt="user-avatar" class="d-block rounded" height="100" width="100" id="uploadedAvatar">
               <div class="button-wrapper">
@@ -44,45 +48,33 @@ $this->load->view('user/includes/header');
                 <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
               </div>
             </div>
-          </div>
+          </div> -->
           <hr class="my-0">
           <div class="card-body">
             <form id="formAccountSettings" method="POST" onsubmit="return false">
               <div class="row">
                 <div class="mb-3 col-md-6">
                   <label for="firstName" class="form-label">First Name</label>
-                  <input class="form-control" type="text" id="firstName" name="firstName" value="John" autofocus="">
+                  <input readonly class="form-control" type="text" id="firstName" name="firstName" value="<?= @$res[0]['first_name']?>" autofocus="">
                 </div>
                 <div class="mb-3 col-md-6">
                   <label for="lastName" class="form-label">Last Name</label>
-                  <input class="form-control" type="text" name="lastName" id="lastName" value="Doe">
+                  <input readonly class="form-control" type="text" name="lastName" id="lastName" value="<?= @$res[0]['last_name']?>">
                 </div>
                 <div class="mb-3 col-md-6">
                   <label for="email" class="form-label">E-mail</label>
-                  <input class="form-control" type="text" id="email" name="email" value="john.doe@example.com" placeholder="john.doe@example.com">
-                </div>
-                <div class="mb-3 col-md-6">
-                  <label for="organization" class="form-label">Organization</label>
-                  <input type="text" class="form-control" id="organization" name="organization" value="ThemeSelection">
+                  <input readonly class="form-control" type="text" id="email" name="email" value="<?= @$res[0]['email']?>" placeholder="john.doe@example.com">
                 </div>
                 <div class="mb-3 col-md-6">
                   <label class="form-label" for="phoneNumber">Phone Number</label>
                   <div class="input-group input-group-merge">
                     <span class="input-group-text">US (+1)</span>
-                    <input type="text" id="phoneNumber" name="phoneNumber" class="form-control" placeholder="202 555 0111">
+                    <input type="text" id="phoneNumber" name="phoneNumber" class="form-control" value="<?= @$res[0]['number']?>" placeholder="202 555 0111">
                   </div>
                 </div>
                 <div class="mb-3 col-md-6">
                   <label for="address" class="form-label">Address</label>
                   <input type="text" class="form-control" id="address" name="address" placeholder="Address">
-                </div>
-                <div class="mb-3 col-md-6">
-                  <label for="state" class="form-label">State</label>
-                  <input class="form-control" type="text" id="state" name="state" placeholder="California">
-                </div>
-                <div class="mb-3 col-md-6">
-                  <label for="zipCode" class="form-label">Zip Code</label>
-                  <input type="text" class="form-control" id="zipCode" name="zipCode" placeholder="231465" maxlength="6">
                 </div>
                 <div class="mb-3 col-md-6">
                   <label class="form-label" for="country">Country</label>
@@ -112,16 +104,6 @@ $this->load->view('user/includes/header');
                     <option value="United Arab Emirates">United Arab Emirates</option>
                     <option value="United Kingdom">United Kingdom</option>
                     <option value="United States">United States</option>
-                  </select>
-                </div>
-                <div class="mb-3 col-md-6">
-                  <label for="language" class="form-label">Language</label>
-                  <select id="language" class="select2 form-select">
-                    <option value="">Select Language</option>
-                    <option value="en">English</option>
-                    <option value="fr">French</option>
-                    <option value="de">German</option>
-                    <option value="pt">Portuguese</option>
                   </select>
                 </div>
                 <div class="mb-3 col-md-6">
