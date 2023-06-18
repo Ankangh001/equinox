d76e9fbe72b249f349f0cd3105d57bcd0da1c09d
Ankangh001 <Ankanghosh010@gmail.com>
2023-06-18 16:02:01 +0530

    update by ankana

--- application/views/admin/includes/header.php
+++ application/views/admin/includes/header.php
@@ -14,7 +14,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Equinox User Dashboard</title>
+    <title>ETC-Admin area</title>
 
     <meta name="description" content="" />
 
