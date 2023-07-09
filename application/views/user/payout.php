db97a4c4e3b3c3670ea1a6a29266906128c97de7
Ankangh001 <Ankanghosh010@gmail.com>
2023-07-09 19:22:21 +0530

    peding updte

--- application/views/user/payout.php
+++ application/views/user/payout.php
@@ -478,7 +478,7 @@ $this->load->view('user/includes/header');
             data: null,
             render: function (data, type, row) {
                 return `${
-                  row.payout_status == 0 ? '<span class="badge bg-label-warning">PEDING</span>' : 
+                  row.payout_status == 0 ? '<span class="badge bg-label-warning">PENDING</span>' : 
                   (row.payout_status == 1 ? '<span class="badge bg-label-success">APPROVED</span>' : 
                     (row.payout_status == 2 ? '<span class="badge bg-label-danger">DENIED</span>' :'')
                   )
